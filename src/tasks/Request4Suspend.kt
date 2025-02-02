cf3f4d10623b6c17eca017b84947a9ab9fd5406c
Sandroisu <sandroisu1411@gmail.com>
2025-02-02 20:40:12 +0300

    task 4 completed

--- src/tasks/Request4Suspend.kt
+++ src/tasks/Request4Suspend.kt
@@ -3,5 +3,16 @@ package tasks
 import contributors.*
 
 suspend fun loadContributorsSuspend(service: GitHubService, req: RequestData): List<User> {
-    TODO()
+    val repos = service.getOrgRepos(req.org).body()
+    val users = mutableListOf<User>()
+    repos?.forEach {
+        val usersFromRequest = service.getRepoContributors(
+            owner = req.org,
+            repo = it.name,
+        ).body()
+        usersFromRequest?.let { remoteUsers ->
+            users.addAll(remoteUsers)
+        }
+    }
+    return users.aggregate()
 }
