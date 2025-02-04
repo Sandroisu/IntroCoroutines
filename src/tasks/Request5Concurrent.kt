2975cd61b21542c654d4cf2018e119cec6601c64
Sandroisu <sandroisu1411@gmail.com>
2025-02-04 22:00:46 +0300

    in middle of 5th excercise

--- src/tasks/Request5Concurrent.kt
+++ src/tasks/Request5Concurrent.kt
@@ -3,11 +3,29 @@ package tasks
 import contributors.*
 import kotlinx.coroutines.*
 
-suspend fun loadContributorsConcurrent(service: GitHubService, req: RequestData): List<User> = coroutineScope {
+suspend fun loadContributorsConcurrent(service: GitHubService, req: RequestData): List<User>  {
+   return loadContributorNotCancellable(service, req)
+    //val repos = service.getOrgRepos(req.org).bodyList()
+    //val usersDeferred = repos.map { repo ->
+    //    async {
+    //        log("starting loading for ${repo.name}")
+    //        delay(3000)
+    //        service.getRepoContributors(
+    //            req.org,
+    //            repo.name
+    //        ).also { logUsers(repo, it) }.bodyList()
+    //    }
+    //}
+    //val users = usersDeferred.awaitAll().flatten().aggregate()
+    //users
+}
+
+suspend fun loadContributorNotCancellable(service: GitHubService, req: RequestData): List<User> {
     val repos = service.getOrgRepos(req.org).bodyList()
     val usersDeferred = repos.map { repo ->
-        async {
+        GlobalScope.async {
             log("starting loading for ${repo.name}")
+            delay(3000)
             service.getRepoContributors(
                 req.org,
                 repo.name
@@ -15,5 +33,5 @@ suspend fun loadContributorsConcurrent(service: GitHubService, req: RequestData)
         }
     }
     val users = usersDeferred.awaitAll().flatten().aggregate()
-    users
+    return users
 }
