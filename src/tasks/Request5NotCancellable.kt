83d53a2e50b65226bcbb4736c24a112073679cf7
Sandroisu <sandroisu1411@gmail.com>
2025-02-09 19:21:43 +0300

    sequential progress update

--- src/tasks/Request5NotCancellable.kt
+++ src/tasks/Request5NotCancellable.kt
@@ -5,5 +5,17 @@ import kotlinx.coroutines.*
 import kotlin.coroutines.coroutineContext
 
 suspend fun loadContributorsNotCancellable(service: GitHubService, req: RequestData): List<User> {
-    TODO()
+    val repos = service.getOrgRepos(req.org).bodyList()
+    val usersDeferred = repos.map { repo ->
+        GlobalScope.async {
+            log("starting loading for ${repo.name}")
+            delay(3000)
+            service.getRepoContributors(
+                req.org,
+                repo.name
+            ).also { logUsers(repo, it) }.bodyList()
+        }
+    }
+    val users = usersDeferred.awaitAll().flatten().aggregate()
+    return users
 }
--- src/tasks/Request6Progress.kt
+++ src/tasks/Request6Progress.kt
@@ -7,5 +7,17 @@ suspend fun loadContributorsProgress(
     req: RequestData,
     updateResults: suspend (List<User>, completed: Boolean) -> Unit
 ) {
-    TODO()
+    val repos = service.getOrgRepos(req.org).body()
+    val users = mutableListOf<User>()
+    repos?.forEach {
+        val usersFromRequest = service.getRepoContributors(
+            owner = req.org,
+            repo = it.name,
+        ).body()
+        usersFromRequest?.let { remoteUsers ->
+            users.addAll(remoteUsers)
+            updateResults(users.aggregate(), false)
+        }
+    }
+    return updateResults(users.aggregate(), true)
 }
