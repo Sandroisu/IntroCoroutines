2419ef0af00267ad959f139cd0a468c6016e1c38
Sandroisu <sandroisu1411@gmail.com>
2025-02-10 21:18:15 +0300

    finished task 7

--- src/tasks/Request5Concurrent.kt
+++ src/tasks/Request5Concurrent.kt
@@ -3,27 +3,10 @@ package tasks
 import contributors.*
 import kotlinx.coroutines.*
 
-suspend fun loadContributorsConcurrent(service: GitHubService, req: RequestData): List<User>  {
-   return loadContributorNotCancellable(service, req)
-    //val repos = service.getOrgRepos(req.org).bodyList()
-    //val usersDeferred = repos.map { repo ->
-    //    async {
-    //        log("starting loading for ${repo.name}")
-    //        delay(3000)
-    //        service.getRepoContributors(
-    //            req.org,
-    //            repo.name
-    //        ).also { logUsers(repo, it) }.bodyList()
-    //    }
-    //}
-    //val users = usersDeferred.awaitAll().flatten().aggregate()
-    //users
-}
-
-suspend fun loadContributorNotCancellable(service: GitHubService, req: RequestData): List<User> {
+suspend fun loadContributorsConcurrent(service: GitHubService, req: RequestData): List<User>  = coroutineScope{
     val repos = service.getOrgRepos(req.org).bodyList()
     val usersDeferred = repos.map { repo ->
-        GlobalScope.async {
+        async {
             log("starting loading for ${repo.name}")
             delay(3000)
             service.getRepoContributors(
@@ -33,5 +16,6 @@ suspend fun loadContributorNotCancellable(service: GitHubService, req: RequestDa
         }
     }
     val users = usersDeferred.awaitAll().flatten().aggregate()
-    return users
+    users
 }
+
